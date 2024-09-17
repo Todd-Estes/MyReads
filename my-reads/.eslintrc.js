277f0278c7029089afa1931d29af2f747962dffd
todd-estes <elestes@gmail.com>
2024-09-17 11:12:51 -0400

    chore: Add eslint dev dependencies and scripts

---
+++ my-reads/.eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y", "import"],
+  rules: {
+    // React rules
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/react-in-jsx-scope": "off", // Not needed in React 17+
+    "react/prop-types": "error",
+
+    // General JavaScript rules
+    "no-unused-vars": "warn",
+    "no-console": "off",
+
+    // Import rules
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "never",
+      },
+    ],
+
+    // Accessibility rules
+    "jsx-a11y/anchor-is-valid": "warn",
+
+    // Stylistic rules
+    indent: ["error", 2],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "comma-dangle": ["error", "only-multiline"],
+    "no-trailing-spaces": [
+      "error",
+      {
+        skipBlankLines: false,
+        ignoreComments: false,
+      },
+    ],
+    "eol-last": ["error", "always"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
